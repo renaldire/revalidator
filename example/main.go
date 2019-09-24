f420b624e5bffcf7320477ede80a43e405252f31
Renaldi <renaldi@amarbank.co.id>
2019-09-24 15:44:32 +0700

    Update main.go

--- example/main.go
+++ example/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 	Validator "revalidator"
+	"time"
 )
 
 func main(){
-	name:="john doe"
-	age:=19
-	email:="john@mail.com"
-	birthday:="1997-03-30"
-	gender:="male"
-	payment_type:="test"
+	name:=""
+	age:=10
+	email:="asd"
+	birthday:="1997-03-30asd"
+	gender:=""
+	payment_type:=""
 	cc_number:=""
+
+	start:=time.Now()
 	validator:=map[string]Validator.Rules{
 		"name":{name,"required|min:5|max:10"},
 		"age":{age,"required|numeric|min:18"},
@@ -23,7 +26,10 @@ func main(){
 		"credit card number":{cc_number,"required_if:payment type,cc"},
 	}
 	err:= Validator.Validate(validator)
+	end:=time.Now()
 
+	execution_time:=end.Sub(start)
+	fmt.Println("Run Time :",execution_time)
 	if err!=nil {
 		fmt.Println(err[0].Error())
 		return
